c1609ecc0a6c67ac2a4a5bbb0b0848f015e66b11
Pramod Dange <pramodsdange1@gmail.com>
2024-02-07 00:17:11 +0530

    Done

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,14 +2,17 @@ package com.driver;
 
 public class F1 extends Car {
 
-    public F1(String name, boolean isManual) {
+    public F1(String name, boolean isManual)
+    {
         //Use arbitrary values for parameters which are not mentioned
         super(name,isManual);
     }
 
-    public void accelerate(int rate) {
-        int newSpeed =  getCurrentSpeed()+ rate;
-        //set the value of new speed by using currentSpeed and rate
+    public void accelerate(int rate)
+    {
+        this.setCurrentSpeed(this.getCurrentSpeed()+rate);
+        int newSpeed = this.getCurrentSpeed(); //set the value of new speed by using currentSpeed and rate
+
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -19,32 +22,45 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-        if (newSpeed == 0) {
-            changeGear(1);
-            this.stop();
-        } else if (newSpeed <= 50) {
-            changeGear(1);
-        } else if (newSpeed <= 100) {
-            changeGear(2);
-        } else if (newSpeed <= 150) {
-            changeGear(3);
-        } else if (newSpeed <= 200) {
-            changeGear(4);
-        } else if (newSpeed <= 250) {
-            changeGear(5);
-        } else {
-            changeGear(6);
-        }
-
         //for all other cases, change the gear accordingly
 
-        if(newSpeed > 0) {
-        changeSpeed(newSpeed, getCurrentDirection());
-        System.out.println("Accelerating - The speed is now: " + newSpeed + ", and the car is in gear: " + getCurrentGear());
-    } else {
-        System.out.println("Car has stopped.");
-    }
+
+
+
+        if(newSpeed == 0)
+        {
+            //Stop the car, set gear as 1
+            this.setCurrentGear(1);
+            this.stop();
 
         }
+        else if(newSpeed>=1 && newSpeed<=50)
+        {
+            this.setCurrentGear(1);
+
+        }
+        else if(newSpeed>=51 && newSpeed<=100)
+        {
+            this.setCurrentGear(2);
+        }
+        else if(newSpeed>=101 && newSpeed<=150)
+        {
+            this.setCurrentGear(3);
+        }
+        else if(newSpeed>=151 && newSpeed<=200)
+        {
+            this.setCurrentGear(4);
+        }
+        else if(newSpeed>=201 && newSpeed<=250)
+        {
+            this.setCurrentGear(5);
+        }
+        else if(newSpeed>=251)
+        {
+            this.setCurrentGear(6);
+        }
+
+        this.move(newSpeed,this.getCurrentDirection());
     }
+}
 
